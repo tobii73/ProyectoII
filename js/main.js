89b6857f901657381a14f72f34aba11ca6501c43
Matheo Such <matheosuch1@gmail.com>
2024-03-23 16:58:28 -0300

    Creacion del boton de busqueda ye estilos

--- js/main.js
+++ js/main.js
@@ -23,3 +23,42 @@ $(document).ready(function () {
 			});
 	});
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+	const inputBuscar = document.getElementById('inputBuscar');
+	const btnBuscar = document.getElementById('btnBuscar');
+
+	btnBuscar.addEventListener('click', function () {
+		buscar();
+	});
+
+	inputBuscar.addEventListener('keypress', function (e) {
+		if (e.key === 'Enter') {
+			buscar();
+		}
+	});
+
+	function buscar() {
+		const textoBusqueda = inputBuscar.value.trim().toLowerCase();
+
+		// Verificar si el texto de búsqueda coincide con alguna de tus categorías o productos
+		if (
+			textoBusqueda === 'Mochilas casuales' ||
+			textoBusqueda === 'Bolsos' ||
+			textoBusqueda === 'Mochilas de trekking'
+		) {
+			// Redirigir a la página correspondiente a la categoría
+			window.location.href = `/pages/mochilasCas.html${textoBusqueda}.html`;
+		} else if (
+			textoBusqueda === 'producto1' ||
+			textoBusqueda === 'producto2' ||
+			textoBusqueda === 'producto3'
+		) {
+			// Redirigir a la página correspondiente al producto
+			window.location.href = `/pagina_de_${textoBusqueda}.html`;
+		} else {
+			// Texto de búsqueda no reconocido
+			alert('No se encontró la categoría o producto buscado.');
+		}
+	}
+});
